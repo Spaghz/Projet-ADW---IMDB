d136629a3e4aaf098143ab789cfe8f4e2823c007
Spaghz <Johnspagh@gmail.com>
2015-05-20 20:21:11 +0200

    script bdd

--- adwproject.sql
+++ adwproject.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- GÃ©nÃ©rÃ© le :  Dim 05 Avril 2015 Ã  21:04
+-- Généré le :  Mer 20 Mai 2015 à 20:03
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de donnÃ©es :  `adwproject`
+-- Base de données :  `adwprojectpublish`
 --
 
 -- --------------------------------------------------------
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `award` (
   `idCelebrity` int(11) NOT NULL,
   `idMovie` int(11) NOT NULL,
-  `awardName` varchar(64) NOT NULL,
-  `awardDate` date NOT NULL,
+  `awardName` varchar(256) NOT NULL,
+  `awardDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`idCelebrity`,`idMovie`,`awardName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -49,7 +49,19 @@ CREATE TABLE IF NOT EXISTS `celebrity` (
   `pictureURI` varchar(64) NOT NULL,
   `rank` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `celebrityupdates`
+--
+
+CREATE TABLE IF NOT EXISTS `celebrityupdates` (
+  `updateNumber` int(11) NOT NULL AUTO_INCREMENT,
+  `celebrityId` int(11) NOT NULL,
+  PRIMARY KEY (`updateNumber`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -63,8 +75,28 @@ CREATE TABLE IF NOT EXISTS `movies` (
   `releaseDate` date NOT NULL,
   `cost` int(11) NOT NULL,
   `posterURI` varchar(32) NOT NULL,
-  `idDirector` int(11) NOT NULL,
+  `idDirector` int(11) DEFAULT NULL,
+  `synopsis` text NOT NULL,
+  `rank` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `movieupdates`
+--
+
+CREATE TABLE IF NOT EXISTS `movieupdates` (
+  `updateNumber` int(11) NOT NULL AUTO_INCREMENT,
+  `movieId` int(11) NOT NULL,
+  `title` varchar(64) NOT NULL,
+  `releaseDate` date NOT NULL,
+  `cost` int(11) NOT NULL,
+  `posterURI` varchar(64) NOT NULL,
+  `idDirector` int(11) DEFAULT NULL,
+  `synopsis` text NOT NULL,
+  PRIMARY KEY (`updateNumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -81,7 +113,7 @@ CREATE TABLE IF NOT EXISTS `news` (
   `illustration` varchar(64) NOT NULL,
   `date` timestamp NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -98,6 +130,19 @@ CREATE TABLE IF NOT EXISTS `play` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `playupdates`
+--
+
+CREATE TABLE IF NOT EXISTS `playupdates` (
+  `updateNumber` int(11) NOT NULL AUTO_INCREMENT,
+  `idCelebrity` int(11) NOT NULL,
+  `idMovie` int(11) NOT NULL,
+  PRIMARY KEY (`updateNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `produce`
 --
 
@@ -105,6 +150,19 @@ CREATE TABLE IF NOT EXISTS `produce` (
   `idCelebrity` int(11) NOT NULL,
   `idMovie` int(11) NOT NULL,
   PRIMARY KEY (`idCelebrity`,`idMovie`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rates`
+--
+
+CREATE TABLE IF NOT EXISTS `rates` (
+  `idUser` int(11) NOT NULL,
+  `idMovie` int(11) NOT NULL,
+  `rate` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idUser`,`idMovie`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -119,7 +177,7 @@ CREATE TABLE IF NOT EXISTS `rights` (
   `isAdmin` tinyint(1) NOT NULL,
   `isPro` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
@@ -139,7 +197,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `pictureURI` varchar(32) DEFAULT NULL,
   `subscribeDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
